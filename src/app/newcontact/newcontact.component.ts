2fee4b1ac9820b095776713ef7b58d3143a576e6
dudua <ali.walat@mediatec.net>
2024-11-01 11:00:23 +0100

    added h1 tag

---
+++ src/app/newcontact/newcontact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newcontact',
+  standalone: true,
+  imports: [],
+  templateUrl: './newcontact.component.html',
+  styleUrl: './newcontact.component.scss'
+})
+export class NewcontactComponent {
+
+}
